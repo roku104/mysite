97099f5676c35431d2509bae5b70b12a092446c0
roku <rktc23@gmail.com>
2018-09-29 16:10:57 +0900

    update

--- config/wsgi.py
+++ config/wsgi.py
@@ -20,5 +20,5 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "アプリ名.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 application = Cling(get_wsgi_application())
